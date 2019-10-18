0cc2ed10ff47eaa804c7f59fb17bbc258e1605ee
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2019-10-18 18:18:11 +0900

    Build error

--- unpack-prebuilt-keystone.sh
+++ unpack-prebuilt-keystone.sh
@@ -1,7 +1,9 @@
 #/bin/bash -x
 
+if ! [ -d build-keystone ]; then
   if ! [ -f keystone-prebuilt.tar.gz ]; then
     wget http://${GITLAB_HOST}/vc707/tee-ta-reference/raw/master/keystone-prebuilt.tar.gz
   fi
-  tar -xzvf keystone-prebuilt.tar.gz
+  tar -xzf keystone-prebuilt.tar.gz
   echo "Unpacked keystone"
+fi
