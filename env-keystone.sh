9ef1b6c2e7a8249b53c802012e7796ac1e1131fd
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2019-12-25 12:41:58 +0900

    Use env

--- env-keystone.sh
+++ env-keystone.sh
@@ -1,4 +1,6 @@
-export GITLAB_HOST=150.82.217.189
+if [ -z "$GITLAB_HOST" ]; then
+  export GITLAB_HOST=192.168.100.100
+fi
 export KEYSTONE_DIR=`pwd`/build-keystone
 export KEYSTONE_SDK_DIR=${KEYSTONE_DIR}/sdk
 export PATH=${PATH}:${KEYSTONE_DIR}/riscv/bin
