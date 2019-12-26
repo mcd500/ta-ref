b7b745258df52389ac2e7a2b66d10d5a93459987
Kaz Kojima <kazumoto.kojima@aist.go.jp>
2019-12-26 17:44:20 +0900

    Add commemt to ref-ta/keystone/Enclave/strcmp.c

--- ref-ta/keystone/Enclave/strcmp.c
+++ ref-ta/keystone/Enclave/strcmp.c
@@ -1,6 +1,9 @@
 #include "Enclave_t.h"
 
 #ifdef EDGER_MACROS_H
+// Adding strcmp for lib/mbed-crypto/library/cipher.c when keedger8r is used.
+// keyedger adds strcmp and several other miscellaneous functions in
+// keyedge/target/include/common_header.h, ATM. It may be temporary.
 int strcmp(const char *s1, const char *s2)
 {
   return __builtin_strcmp(s1, s2);
