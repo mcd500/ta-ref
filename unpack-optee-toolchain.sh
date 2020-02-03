2991ea006b9a5a0352db8429c9a611ed5c5772d9
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-02-03 18:14:07 +0900

    Fix pointing wrong directory for toolchain

--- unpack-optee-toolchain.sh
+++ unpack-optee-toolchain.sh
@@ -10,10 +10,10 @@ wget ${URL}:2000/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
 wget ${URL}:2000/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
 
 cd aarch32
-tar xf ../gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz || exit 1
+tar xf ../gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz --strip-components=1 || exit 1
 cd ..
 
 cd aarch64
-tar xf ../gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz || exit 1
+tar xf ../gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz --strip-components=1 || exit 1
 cd ..
 
