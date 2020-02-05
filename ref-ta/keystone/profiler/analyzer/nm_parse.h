2b45f0963c5ce392f151cfdbb5f97a54c8dcb1bf
knknkn1162 <knknkn1162@gmail.com>
2020-02-05 09:53:37 +0900

    use hash table

--- ref-ta/keystone/profiler/analyzer/nm_parse.h
+++ ref-ta/keystone/profiler/analyzer/nm_parse.h
@@ -1,9 +1,19 @@
 #pragma once
 #include <stddef.h>
+#include <stdlib.h>
+
+#define HASH_SIZE 8192
 
 struct nm_info {
     char type;
     char func_name[256];
 };
 
-int parse_nm(const char *fname, struct nm_info* out, size_t size);
+struct list {
+    struct list *next;
+    unsigned long addr;
+    struct nm_info* nm;
+};
+
+const char* get_func_name(struct list *table, unsigned long addr);
+struct list* parse_nm(const char *fname);
