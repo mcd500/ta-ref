4cb3c7eaeb418ee42619afaac8453f99474bd25f
knknkn1162 <knknkn1162@gmail.com>
2020-02-10 15:18:36 +0900

    support ENCLAVE_PERF_WRAPPER and make perf in optee

--- ref-ta/op-tee/run-apps.sh
+++ ref-ta/op-tee/run-apps.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-xterm -e ./run-record.sh&
+./run-record.sh&
 sleep 2
 
+if [ $# -eq 1 ] && [ $1 = "perf" ]; then
+
 expect -c '
-    set timeout 50
+set timeout 50
 
-    spawn ./run-qemu.sh
+spawn ./run-qemu.sh
 
-    expect "*?ogin" { send "root\r" }
+expect "*?ogin" { send "root\r" }
 
-    expect "# " { send "optee_ref_ta\r" }
-    expect "# " { send "ls -ls\r" }
-    expect "# " { send "./analyzer shared_mem enclave_nm\r" }
+expect "# " { send "optee_ref_ta\r" }
+expect "# " { send "ls -ls\r" }
+expect "# " { send "./analyzer shared_mem enclave_nm\r" }
+expect "# " { send "poweroff\r" }
 
-    expect "# " { send "poweroff\r" }
+expect "*?RROR:   QEMU System Off: operation not handled." { send "\001c" }
+'
 
-    expect "*?RROR:   QEMU System Off: operation not handled." { send "\001c" }
-    '
+
+else
+expect -c '
+set timeout 50
+
+spawn ./run-qemu.sh
+
+expect "*?ogin" { send "root\r" }
+
+expect "# " { send "optee_ref_ta\r" }
+expect "# " { send "poweroff\r" }
+
+expect "*?RROR:   QEMU System Off: operation not handled." { send "\001c" }
+'
+
+fi
 
 ./cut.py
 
