847ba1c82ae0f0915dd587912edf4dde336f3401
knknkn1162 <knknkn1162@gmail.com>
2020-02-14 11:01:22 +0900

    fix optee-rpi-*.sh

--- scripts/optee-rpi3-ssh-clean.sh
+++ scripts/optee-rpi3-ssh-clean.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-expect -c '
+expect -c "
     set timeout 30
     spawn ssh gitlab@${RPI3_IP_ADDR}:/home/gitlab
-    expect "*$ " { send "sudo rm -rf ./output\r" }
+    expect \"*$ \" { send \"sudo rm -rf ./output\r\" }
     expect eof
-'
-
+"
--- scripts/optee-rpi3-ssh-test.sh
+++ scripts/optee-rpi3-ssh-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-expect -c '
+scp -r ./output gitlab@${RPI3_IP_ADDR}:/home/gitlab
+expect -c "
     set timeout 30
     spawn ssh gitlab@${RPI3_IP_ADDR}:/home/gitlab/output
-    expect "*$ " { send "sudo cp -r lib/optee_armtz/* /lib/optee_armtz/\r" }
-    expect "*$ " { send "sudo usr/bin/optee_ref_ta\r" }
-    expect "*$ "  { send "./root/analyzer ./shared_mem ./root/enclave_nm\r" }
-    expect "* Invoking all ref tests in Enclave"
+    expect \"*$ \" { send \"sudo cp -r lib/optee_armtz/* /lib/optee_armtz/\r\" }
+    expect \"*$ \" { send \"sudo usr/bin/optee_ref_ta\r\" }
+    expect \"*$ \"  { send \"./root/analyzer ./shared_mem ./root/enclave_nm\r\" }
+    expect \"* Invoking all ref tests in Enclave\"
     expect eof
-'
-
+"
