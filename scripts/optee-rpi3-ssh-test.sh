b2ca23b421fd91b07d5c52a758839d8f3a128a4d
knknkn1162 <knknkn1162@gmail.com>
2020-02-14 11:15:13 +0900

    fix script

--- scripts/optee-rpi3-ssh-test.sh
+++ scripts/optee-rpi3-ssh-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-scp -r ./output gitlab@${RPI3_IP_ADDR}:/home/gitlab
+scp -r ./${OUTPUT_DIR} gitlab@${RPI3_IP_ADDR}:/home/gitlab
 expect -c "
     set timeout 30
-    spawn ssh gitlab@${RPI3_IP_ADDR}:/home/gitlab/output
+    spawn ssh gitlab@${RPI3_IP_ADDR}
+    expect \"*$ \"  { send \"cd ${OUTPUT_DIR}\r\" }
     expect \"*$ \" { send \"sudo cp -r lib/optee_armtz/* /lib/optee_armtz/\r\" }
     expect \"*$ \" { send \"sudo usr/bin/optee_ref_ta\r\" }
     expect \"*$ \"  { send \"./root/analyzer ./shared_mem ./root/enclave_nm\r\" }
