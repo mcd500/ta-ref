c46395afcaa387e19299dab483b87e7a18175616
knknkn1162 <knknkn1162@gmail.com>
2020-03-02 17:55:25 +0900

    enhance config.mk and platform build

--- build/config.mk
+++ build/config.mk
@@ -1,13 +1,13 @@
 .PHONY: all clean mrproper
 
 include ./general.mk
+PLATFORM_LIB=libplatform.a
 
 all:
 	make -C $(TOPDIR)/config CRYPT_TYPE=$(CRYPT_TYPE) INCLUDE_DIR=$(CURDIR)/include
-	$(SLN) $(TOPDIR)/config/*.a ./lib
+	$(SLN) $(TOPDIR)/config/$(PLATFORM_LIB) lib/$(PLATFORM_LIB)
 
 
 clean:
-	$(RM) ./lib/*.a
-
-mrproper: clean
+	$(RM) lib/$(PLATFORM_LIB)
+	make -C $(TOPDIR)/config clean
