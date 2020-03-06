89aa888899287ea315625da7fb30553aeb2e8610
knknkn1162 <knknkn1162@gmail.com>
2020-03-06 10:46:59 +0900

    create image.mk to delegate 'make image'

---
+++ test/keystone/image.mk
@@ -0,0 +1,18 @@
+BUILDROOT_OVERLAY_DIR=${KEYSTONE_DIR}/buildroot_overlay/root
+EYRIE_BIN=$(KEYSTONE_DIR)/sdk/rts/eyrie/eyrie-rt
+APP_BIN=App/App.client
+ENCLAVE_BIN=Enclave/Enclave.eapp_riscv
+
+.PHONY: all clean
+# `make image` uses ${KEYSTONE_DIR} specific toolchains and settings, so we disable expanded settings.
+unexport CC AR CXX LINK AS SLN
+image: ship
+	make -C $(KEYSTONE_DIR) image
+
+# In Keystone, Four items are required to run Enclave program; App.client, Enclave.eapp_riscv, eyrie OS runtime(eyrie-rt) and keystone-driver module.
+ship:
+	install $(EYRIE_BIN) $(APP_BIN) $(ENCLAVE_BIN) $(BUILDROOT_OVERLAY_DIR)
+
+# you should erase the shipped contents in $(BUILDROOT_OVERLAY_DIR) by yourself, because keystone is separated from this project.(In this case, we use docker image to prepare keystone environment.)
+clean:
+	$(RM) $(EYRIE_BIN) $(APP_BIN) $(ENCLAVE_BIN)
