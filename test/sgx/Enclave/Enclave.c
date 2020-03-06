e0804a3ad47fef6b5efbbe0b63d31494c1d20fb3
knknkn1162 <knknkn1162@gmail.com>
2020-03-06 22:38:48 +0900

    add new files in sgx/Enclave

---
+++ test/sgx/Enclave/Enclave.c
@@ -0,0 +1,65 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (C) 2019 National Institute of Advanced Industrial Science
+ *                           and Technology (AIST)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "Enclave.h"
+#include "printf.h"
+
+/* ecall_print_aes:
+ *   testing symmetric key verification
+ */
+void ecall_ta_main(void)
+{
+
+    printf("ecall_ta_main() start\n");
+
+    gp_random_test();
+
+    gp_ree_time_test();
+
+    gp_trusted_time_test();
+
+    gp_secure_storage_test();
+
+    gp_message_digest_test();
+
+    gp_symmetric_key_enc_verify_test();
+
+    gp_symmetric_key_gcm_verify_test();
+
+//    gp_symmetric_key_dec_verify_test();
+
+    gp_asymmetric_key_sign_test();
+
+//    gp_asymmetric_key_verify_test();
+#ifdef NOT_DONE
+#endif
+
+    printf("ecall_ta_main() end\n");
+}
