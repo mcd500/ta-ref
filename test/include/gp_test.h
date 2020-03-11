74cdafdb5cf50e161d802b6926e7f21e657615f8
knknkn1162 <knknkn1162@gmail.com>
2020-03-11 15:22:23 +0900

    change directory configration on test/

--- test/include/gp_test.h
+++ test/include/gp_test.h
@@ -1,9 +1,17 @@
-void gp_random_test();
-void gp_ree_time_test();
-void gp_trusted_time_test();
-void gp_secure_storage_test();
-void gp_message_digest_test();
-void gp_symmetric_key_enc_verify_test();
-void gp_asymmetric_key_sign_test();
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
+void gp_random_test(void);
+void gp_ree_time_test(void);
+void gp_trusted_time_test(void);
+void gp_secure_storage_test(void);
+void gp_message_digest_test(void);
+void gp_symmetric_key_enc_verify_test(void);
+void gp_symmetric_key_gcm_verify_test(void);
+void gp_asymmetric_key_sign_test(void);
 void gp_symmetric_key_gcm_verify_test();
+
+#if defined(__cplusplus)
+}
+#endif
