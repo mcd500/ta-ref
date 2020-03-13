6d88b71ce3dde07017f85517bb3fb8b4a4857ffb
knknkn1162 <knknkn1162@gmail.com>
2020-03-13 16:05:00 +0900

    fix env/optee.sh

--- env/optee.sh
+++ env/optee.sh
@@ -3,9 +3,10 @@
 export TEE=optee
 # this must not be used because optee_os assume that ARCH=arm
 #export ARCH=aarch64
-export TOOLPREFIX=aarch64-linux-gnu-
 export ENABLE_TEE_ENVS=1
 # ${OPTEE_DIR}/toolchains/${ARCH}/bin would error when link.
+export OPTEE_OUT_DIR=${OPTEE_DIR}/out-br
 export OPTEE_TOOLCHAIN_DIR=${OPTEE_DIR}/out-br/host/bin
-export PATH=${OPTEE_TOOLCHAIN_DIR}:${PATH}
+export TOOLPREFIX=${OPTEE_TOOLCHAIN_DIR}/aarch64-linux-gnu-
+
 export TA_DEV_DIR=${OPTEE_DIR}/optee_os/out/arm/export-ta_arm64
