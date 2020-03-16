f3262a7f8e2f613277497a5a0580fd9db801ceb7
knknkn1162 <knknkn1162@gmail.com>
2020-03-16 11:58:37 +0900

    fix run-apps.sh

--- test/optee/run-apps.sh
+++ test/optee/run-apps.sh
@@ -1,46 +1,18 @@
 #!/bin/bash
 
-if [ $# -eq 1 ] && [ $1 = "perf" ]; then
-
-./run-record.sh&
-sleep 2
-
-expect -c '
+expect -c "
 set timeout 50
 
+send_user \"$env(PORT)\n\"
+send_user \"$env(LOG_FILE)\n\"
 spawn ./run-qemu.sh
 
-expect "*?ogin" { send "root\r" }
+expect \"*?ogin\" { send \"root\r\" }
+expect \"# \" { send \"./optee_ref_ta\r\" }
+expect \"Invoking all ref tests in Enclave\"
+expect \"# \" { send \"poweroff\r\" }
 
-expect "# " { send "optee_ref_ta\r" }
-expect "# " { send "ls -ls\r" }
-expect "# " { send "./analyzer shared_mem enclave_nm\r" }
-expect "# " { send "poweroff\r" }
-
-expect "*?RROR:   QEMU System Off: operation not handled." { send "\001c" }
-'
-
-
-else
-
-#xterm -e ./run-record.sh&
-sleep 1
-
-expect -c '
-set timeout 50
-
-spawn ./run-qemu.sh
-
-expect "*?ogin" { send "root\r" }
-
-expect "# " { send "./optee_ref_ta\r" }
-expect "# " { send "poweroff\r" }
-
-expect "*?RROR:   QEMU System Off: operation not handled." { send "\001c" }
-'
-
-fi
-
-#./cut.py
+expect \"*?RROR:   QEMU System Off: operation not handled.\" { send \"\001c\" }
+"
 
 exit
