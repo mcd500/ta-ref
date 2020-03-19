59ba592d105fcc9c05f6f4de3f76c9ead9f3dede
knknkn1162 <knknkn1162@gmail.com>
2020-03-19 18:28:09 +0900

    support NUC on sgx/image.mk

--- test/sgx/image.mk
+++ test/sgx/image.mk
@@ -2,13 +2,15 @@ APP_BIN=App/sgx_app
 ENCLAVE_LIB=Enclave/enclave.signed.so
 ENCLAVE_NM = $(ENCLAVE_LIB:.signed.so=.nm)
 
+OUT_DIR=$(CURDIR)
+OUT_FILES=$(APP_BIN) $(ENCLAVE_LIB)
+
 ifeq ($(PROFILER), ON)
 ANALYZER_BIN=$(TOPDIR)/profiler/analyzer/analyzer
+OUT_FILES += $(ENCLAVE_NM) $(ANALYZER_BIN)
 else
 ANALYZER_BIN=
 endif
-
-OUT_DIR=$(CURDIR)
 
 .PHONY: all clean
 
@@ -17,9 +19,14 @@ TARGET += $(MACHINE)_image
 all: $(TARGET)
 
 SIM_image: SIM_ship
+	install $(OUT_FILES) $(OUT_DIR)
 
 SIM_ship: $(ENCLAVE_NM)
-	install $(APP_BIN) $(ENCLAVE_LIB) $(ENCLAVE_NM) $(ANALYZER_BIN) $(OUT_DIR)
+
+NUC_image: NUC_ship
+
+NUC_ship: $(ENCLAVE_NM)
+	FILES="$(OUT_FILES)" USER=$(TEST_USER) IP_ADDR=$(NUC_IP_ADDR) ./ship-nuc.sh
 
 $(ENCLAVE_NM):
 	nm $(ENCLAVE_LIB) > $(ENCLAVE_NM)
