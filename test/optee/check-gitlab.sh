207ad67df9c798e515af96e7a1d02511783743a8
knknkn1162 <knknkn1162@gmail.com>
2020-03-25 13:45:34 +0900

    refactor test/optee/run.mk

--- test/optee/check-gitlab.sh
+++ test/optee/check-gitlab.sh
@@ -8,7 +8,7 @@ spawn ssh ${USER}@${IP_ADDR}
 
 expect \"*(yes/no)? \" { send \"yes\r\" }
 expect \"*password: \" { send \"gitlab\r\" }
-expect \"*$ \" { send \"dirname $\(readlink /lib/optee_armtz/a6f77c1e-96fe-4a0e-9e74-262582a4c8f1.ta\)\r\" }
+expect \"*$ \" { send \"dirname $\(readlink /lib/optee_armtz/${UUID_TA}\)\r\" }
 expect \"/home/gitlab/out\r\" {} timeout { exit 1 }
 exit
 "
--- test/optee/run.mk
+++ test/optee/run.mk
@@ -4,6 +4,7 @@ TARGETS = $(MACHINE)_demo
 RUN_SCRIPT := ../ssh_script/run-gitlab.sh
 CHECK_SCRIPT := ./check-gitlab.sh
 UUID := a6f77c1e-96fe-4a0e-9e74-262582a4c8f1
+UUID_TA := $(UUID).ta
 UUID_NM := $(UUID).nm
 APP_BIN := optee_ref_ta
 
@@ -22,7 +23,7 @@ RPI3_demo: check_demo
 	COMMAND="$(RPI3_COMMAND)" USER=$(TEST_USER) IP_ADDR=$(RPI3_IP_ADDR) ${RUN_SCRIPT}
 
 check_demo:
-	USER=$(TEST_USER) IP_ADDR=$(RPI3_IP_ADDR) ${CHECK_SCRIPT}
+	USER=$(TEST_USER) IP_ADDR=$(RPI3_IP_ADDR) UUID_TA=$(UUID_TA) ${CHECK_SCRIPT}
 
 socat:
 	socat TCP4-LISTEN:$(PORT),reuseaddr - | tee /dev/stdout &
