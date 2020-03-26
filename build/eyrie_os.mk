277a080b6e2c99dcf9ab01447ac089ecd5326cca
knknkn1162 <knknkn1162@gmail.com>
2020-03-26 17:40:18 +0900

    fix eyrie_os.mk

--- build/eyrie_os.mk
+++ build/eyrie_os.mk
@@ -1,7 +1,5 @@
-ifeq ($(EYRIE_DEBUG_FLAGS), "")
 # TODO: what is env_setup?
 EYRIE_DEBUG_FLAGS=freemem linux_syscall untrusted_io_syscall env_setup
-endif
 
 ifeq ($(DEBUG_TYPE), DEBUG)
 EYRIE_DEBUG_FLAGS += strace_debug debug
