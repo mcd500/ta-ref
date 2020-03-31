53603d74be1d5cdc67aa027487d175e1042d9864
knknkn1162 <knknkn1162@gmail.com>
2020-03-31 13:46:02 +0900

    add optee/Enclave/trace.c

---
+++ test/optee/Enclave/trace.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+// vsnprintf, BUFSIZ
+#include <stdio.h>
+
+#ifdef ENCLAVE_VERBOSE
+#define BUF_SIZE 65536
+char print_buf[BUF_SIZE];
+size_t print_pos;
+#endif
+
+static inline unsigned int _strlen(const char* str)
+{
+  const char* s;
+  for (s = str; *s; ++s);
+  return (unsigned int)(s - str);
+}
+
+// for trace GP API
+int tee_printf(const char* fmt, ...)
+{
+#ifdef ENCLAVE_VERBOSE
+  char *buf = &print_buf[print_pos];
+  va_list ap;
+
+  va_start(ap, fmt);
+  vsnprintf(buf, BUF_SIZE-print_pos, fmt, ap);
+  va_end(ap);
+  int res = (int)_strlen(buf) + 1;
+  print_pos += res-1;
+  printf("%s", buf, print_pos);
+  return res;
+#else
+  return 0;
+#endif
+}
