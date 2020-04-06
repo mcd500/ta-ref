51f8d51db15ef977b2425fab515a9d97d4c86a1e
knknkn1162 <knknkn1162@gmail.com>
2020-04-06 15:01:23 +0900

    fix optee/Enclave in qemu

--- test/optee/scripts/run-qemu.sh
+++ test/optee/scripts/run-qemu.sh
@@ -8,6 +8,7 @@ send_user \"$env(LOG_FILE)\n\"
 spawn ./scripts/launch-qemu.sh
 
 expect \"*?ogin\" { send \"root\r\" }
+expect \"# \" { send \"cd /home/gitlab/out\r\" }
 expect \"# \" { send \"./optee_ref_ta\r\" }
 expect \"*start*\"
 expect \"*succeed*\"
