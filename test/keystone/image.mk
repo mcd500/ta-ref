1e4dfccc1fbbbcf913a940b31f97337ae12b03ed
knknkn1162 <knknkn1162@gmail.com>
2020-05-07 14:30:21 +0900

    support REE profiler in keystone

--- test/keystone/image.mk
+++ test/keystone/image.mk
@@ -5,6 +5,7 @@ EYRIE_BIN=$(KEYSTONE_DIR)/sdk/rts/eyrie/eyrie-rt
 APP_BIN=App/App.client
 ENCLAVE_BIN=Enclave/Enclave.eapp_riscv
 ENCLAVE_NM = $(ENCLAVE_BIN:.eapp_riscv=.nm)
+APP_NM = $(APP_BIN:.client=.nm)
 
 ifeq ($(PROFILER), ON)
 ANALYZER_BIN=$(TOPDIR)/profiler/analyzer/analyzer
@@ -13,7 +14,7 @@ ANALYZER_BIN=
 endif
 
 TARGET = $(MACHINE)_image
-OUT_FILES = $(EYRIE_BIN) $(APP_BIN) $(ENCLAVE_BIN) $(ENCLAVE_NM) $(ANALYZER_BIN) $(KEYSTONE_DRIVER_MODULE)
+OUT_FILES = $(EYRIE_BIN) $(APP_BIN) $(ENCLAVE_BIN) $(ENCLAVE_NM) $(APP_NM) $(ANALYZER_BIN) $(KEYSTONE_DRIVER_MODULE)
 
 SIM_OUT_DIR=$(BUILDROOT_OVERLAY_DIR)/home/gitlab/out
 
@@ -22,7 +23,7 @@ include ./machine.mk
 
 all: $(TARGET)
 
-$(MACHINE)_ship: $(ENCLAVE_NM) $(MACHINE)_clean
+$(MACHINE)_ship: $(NM) $(MACHINE)_clean
 	FILES="$(OUT_FILES)" USER=$(USER) PASSWD=$(PASSWD) IP_ADDR=$(IP_ADDR) $(SHIP_SCRIPT)
 
 $(MACHINE)_clean:
@@ -34,14 +35,19 @@ SIM_image: SIM_ship
 	make -C $(KEYSTONE_DIR)/build image
 
 # In Keystone, Four items are required to run Enclave program; App.client, Enclave.eapp_riscv, eyrie OS runtime(eyrie-rt) and keystone-driver module.
-SIM_ship: $(ENCLAVE_NM)
+SIM_ship: analyzer_nm
 	mkdir -p $(SIM_OUT_DIR)
 	install $(OUT_FILES) $(SIM_OUT_DIR)
+
+analyzer_nm: $(ENCLAVE_NM) $(APP_NM)
 
 $(ENCLAVE_NM):
 	nm $(ENCLAVE_BIN) > $(ENCLAVE_NM)
 
+$(APP_NM):
+	nm $(APP_BIN) > $(APP_NM)
+
 
 # you should erase the shipped contents in $(BUILDROOT_OVERLAY_DIR) by yourself, because keystone is separated from this project.(In this case, we use docker image to prepare keystone environment.)
 clean:
-	$(RM) $(APP_BIN) $(ENCLAVE_BIN) $(ENCLAVE_NM)
+	$(RM) $(APP_BIN) $(ENCLAVE_BIN) $(ENCLAVE_NM) $(APP_NM)
