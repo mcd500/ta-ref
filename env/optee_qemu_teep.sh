2227e468bff846dc9ee7f274460dfad8c543d4f0
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-06-29 18:02:06 +0900

    Use the toolchain provided by optee

--- env/optee_qemu_teep.sh
+++ env/optee_qemu_teep.sh
@@ -9,7 +9,7 @@ export ENABLE_TEE_ENVS=1
 export OPTEE_DIR=build-optee
 # ${OPTEE_DIR}/toolchains/${ARCH}/bin would error when link.
 export OPTEE_OUTBR_DIR=${OPTEE_DIR}/out-br
-export OPTEE_TOOLCHAIN_DIR=${OPTEE_DIR}/out-br/host/bin
+export OPTEE_TOOLCHAIN_DIR=${OPTEE_DIR}/toolchains/aarch64/bin
 export TOOLPREFIX=${OPTEE_TOOLCHAIN_DIR}/aarch64-linux-gnu-
 export TOOLPREFIX32=${OPTEE_DIR}/toolchains/aarch32/bin/arm-linux-gnueabihf-
 
