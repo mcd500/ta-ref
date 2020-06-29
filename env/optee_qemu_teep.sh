85d8265f728d21a2afa3a834a8e40a2c6fc40fb6
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-06-29 17:58:46 +0900

    It has different directory

---
+++ env/optee_qemu_teep.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+export TEE=optee
+export MACHINE=SIM
+# this must not be used because optee_os assume that ARCH=arm
+#export ARCH=aarch64
+export ENABLE_TEE_ENVS=1
+export OPTEE_DIR=build-optee
+# ${OPTEE_DIR}/toolchains/${ARCH}/bin would error when link.
+export OPTEE_OUTBR_DIR=${OPTEE_DIR}/out-br
+export OPTEE_TOOLCHAIN_DIR=${OPTEE_DIR}/out-br/host/bin
+export TOOLPREFIX=${OPTEE_TOOLCHAIN_DIR}/aarch64-linux-gnu-
+export TOOLPREFIX32=${OPTEE_DIR}/toolchains/aarch32/bin/arm-linux-gnueabihf-
+
+export TA_DEV_DIR=${OPTEE_DIR}/optee_os/out/arm/export-ta_arm64
