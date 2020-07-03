82bd5b022d60342099b21bf03f05356177810fd9
knknkn1162 <knknkn1162@gmail.com>
2020-07-03 15:36:24 +0900

    use sshpass for test/scripts/(ship|clean)-gitlab.sh

--- test/scripts/clean-gitlab.sh
+++ test/scripts/clean-gitlab.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 rm -f ~/.ssh/known_hosts
-expect -c "
-set timeout 60
-
-spawn ssh -p ${PORT} ${USER}@${IP_ADDR} /bin/bash -c \'rm -rf /home/gitlab/out && mkdir -p /home/gitlab/out\'
-
-expect \"?\" { send \"yes\r\" }
-expect \"*password: \" { send \"${PASSWD}\r\" }
-expect eof
-"
-
-exit
+sshpass -p ${PASSWD} scp -P ${PORT} -o "StrictHostKeyChecking no" ${USER}@${IP_ADDR} /bin/bash -c 'rm -rf /home/gitlab/out && mkdir -p /home/gitlab/out'
--- test/scripts/ship-gitlab.sh
+++ test/scripts/ship-gitlab.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 rm -f ~/.ssh/known_hosts
-expect -c "
-set timeout 60
-
-spawn scp -P ${PORT} ${FILES} ${USER}@${IP_ADDR}:/home/gitlab/out
-
-expect \"?\" { send \"yes\r\" }
-expect \"*password: \" { send \"${PASSWD}\r\" }
-expect eof
-"
-
-exit
+sshpass -p ${PASSWD} scp -P ${PORT} -o "StrictHostKeyChecking no" ${FILES} ${USER}@${IP_ADDR}:/home/gitlab/out
