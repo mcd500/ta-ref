257a10f3bc57475c7eee0ca3c0e1f0339a9256a5
knknkn1162 <knknkn1162@gmail.com>
2020-07-03 15:51:28 +0900

    bugfix clean-gitlab.sh

--- test/scripts/clean-gitlab.sh
+++ test/scripts/clean-gitlab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f ~/.ssh/known_hosts
-sshpass -p ${PASSWD} scp -P ${PORT} -o "StrictHostKeyChecking no" ${USER}@${IP_ADDR} /bin/bash -c 'rm -rf /home/gitlab/out && mkdir -p /home/gitlab/out'
+sshpass -p ${PASSWD} ssh -p ${PORT} -o "StrictHostKeyChecking no" ${USER}@${IP_ADDR} "rm -rf /home/gitlab/out && mkdir -p /home/gitlab/out"
