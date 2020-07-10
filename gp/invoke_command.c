4270c7d6a6a31fa7a0430a4cf73c738e050c0e5a
knknkn1162 <knknkn1162@gmail.com>
2020-07-10 10:32:50 +0900

    bugfix gp/invoke_command.c; use #if directive

--- gp/invoke_command.c
+++ gp/invoke_command.c
@@ -37,7 +37,7 @@
 #define TEEP_AGENT_TA_INSTALL   2
 #define TEEP_AGENT_TA_DELETE    3
 
-#ifdef TEE==keystone
+#if TEE==keystone
 void runInvokeCommand(invoke_command_t ret_cmd)
 {
     uint32_t type;
