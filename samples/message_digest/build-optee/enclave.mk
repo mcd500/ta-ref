2d3fb449b909555c0f40cfa4b59272a84635792d
kikuchi <kikuchi@lepidum.co.jp>
2022-01-07 19:28:04 +0900

    optee build wip

--- samples/message_digest/build-optee/enclave.mk
+++ samples/message_digest/build-optee/enclave.mk
@@ -1,1 +1,9 @@
-# Called from optee Makefile, for building TA
+BINARY = 8d82573a-926d-4754-9353-32dc29997f74
+CROSS_COMPILE = aarch64-linux-gnu-
+CPPFLAGS = \
+	-I../../../../ta-ref/build/include \
+	-I../../../../ta-ref/build/include/api \
+	-I../../../../ta-ref/build/include/gp \
+	-Dtee_printf=printf
+
+-include $(TA_DEV_DIR)/mk/ta_dev_kit.mk
